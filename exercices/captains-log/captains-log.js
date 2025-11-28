8934050ee9cf57576a7d52b84061b35adc8ba65e
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2025-11-28 13:21:21 +0100

    ex captain

--- exercices/captains-log/captains-log.js
+++ exercices/captains-log/captains-log.js
@@ -6,9 +6,7 @@
  * @returns {string} the generated registry number.
  */
 export function randomShipRegistryNumber() {
-  throw new Error(
-    'Please remove this line and implement the randomShipRegistryNumber() function',
-  );
+  return "NCC-" + Math.floor(Math.random() * (9999 - 1000) + 1) + 1000; //Honnêtement, notation de Math.random -1/10
 }
 
 /**
@@ -17,18 +15,15 @@ export function randomShipRegistryNumber() {
  * @returns {number} a stardate between 41000 (inclusive) and 42000 (exclusive).
  */
 export function randomStardate() {
-  throw new Error(
-    'Please remove this line and implement the randomStardate() function',
-  );
+  return Math.random() * (42000 - 41000) + 41000;
 }
 
 /**
  * Generates a random planet class.
- *
+ *D, H, J, K, L, M, N, R, T, and Y.
  * @returns {string} a one-letter planet class.
  */
 export function randomPlanetClass() {
-  throw new Error(
-    'Please remove this line and implement the randomStardate() function',
-  );
+  let list = ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"];
+  return list[Math.floor(Math.random() * list.length)];
 }
