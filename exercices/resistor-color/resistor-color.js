8d0147317da967c2370d6d9dfea1a2c4ec5b7668
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2025-12-05 13:39:05 +0100

    Update resistor-color.js

--- exercices/resistor-color/resistor-color.js
+++ exercices/resistor-color/resistor-color.js
@@ -5,54 +5,20 @@
  * @returns {number}
  */
 export const colorCode = (color) => {
-  let lowercolor = color.toLowerCase();
-  let code;
-  switch (lowercolor) {
-    case "black": {
-      code = 0;
-      break;
-    }
-    case "brown": {
-      code = 1;
-      break;
-    }
-    case "red": {
-      code = 2;
-      break;
-    }
-    case "orange": {
-      code = 3;
-      break;
-    }
-    case "yellow": {
-      code = 4;
-      break;
-    }
-    case "green": {
-      code = 5;
-      break;
-    }
-    case "blue": {
-      code = 6;
-      break;
-    }
-    case "violet": {
-      code = 7;
-      break;
-    }
-    case "grey": {
-      code = 8;
-      break;
-    }
-    case "white": {
-      code = 9;
-      break;
-    }
-    default: {
-      code = null;
-    }
-  }
-  return code;
+  const key = color.toLowerCase();
+  return COLOR_TABLE[key] ?? null;
+};
+export const COLOR_TABLE = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
 };
 
 export const COLORS = [
