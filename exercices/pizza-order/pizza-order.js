0d281f3030f2bd04fc7e1ebf232fb647cd9408ae
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2025-12-12 15:54:51 +0100

    Update pizza-order.js

--- exercices/pizza-order/pizza-order.js
+++ exercices/pizza-order/pizza-order.js
@@ -11,32 +11,29 @@
  * @returns {number} the price of the pizza
  */
 export function pizzaPrice(pizza, ...extras) {
-
-const ORDER = [pizza, ...extras
-];
-let price = 0;
-for (let i = 0; i < ORDER.length; i++) {
-  switch (ORDER[i]) {
-    case 'Margherita':
-      price += 7;
-      break;
-    case 'Caprese':
-      price += 9;
-      break;
-    case 'Formaggio':
-      price += 10;
-      break;
-    case 'ExtraToppings':
-      price += 2;
-      break;
-    case 'ExtraSauce':
-      price += 1;
-      break;
-    default:
-      price += 0;
+  const ORDER = [pizza, ...extras];
+  let price = 0;
+  for (let i = 0; i < ORDER.length; i++) {
+    switch (ORDER[i]) {
+      case "Margherita":
+        price += 7;
+        break;
+      case "Caprese":
+        price += 9;
+        break;
+      case "Formaggio":
+        price += 10;
+        break;
+      case "ExtraToppings":
+        price += 2;
+        break;
+      case "ExtraSauce":
+        price += 1;
+        break;
+      default:
+        price += 0;
+    }
   }
-
-}
 
   return price;
 }
@@ -51,11 +48,11 @@ for (let i = 0; i < ORDER.length; i++) {
  * @returns {number} the price of the total order
  */
 export function orderPrice(pizzaOrders) {
- let total = 0;
- 
+  let total = 0;
+
   for (const order of pizzaOrders) {
     total += pizzaPrice(order.pizza, ...order.extras);
   }
- 
+
   return total;
 }
