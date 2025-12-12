3cfa741ab92742f5e3d2b34d44b2d13d9708e6af
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2025-12-12 13:02:11 +0100

    Update resistor-color.js

--- exercices/resistor-color/resistor-color.js
+++ exercices/resistor-color/resistor-color.js
@@ -6,21 +6,8 @@
  */
 export const colorCode = (color) => {
   const key = color.toLowerCase();
-  return COLOR_TABLE[key] ?? null;
+  return COLORS.indexOf(key) ?? null;
 };
-export const COLOR_TABLE = {
-  black: 0,
-  brown: 1,
-  red: 2,
-  orange: 3,
-  yellow: 4,
-  green: 5,
-  blue: 6,
-  violet: 7,
-  grey: 8,
-  white: 9,
-};
-
 export const COLORS = [
   "black",
   "brown",
